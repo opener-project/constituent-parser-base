9f08bae7cbabeb9597eeb55892eaff5f7457a91b
sb-olr <sb-olr@opener>
2013-03-28 00:00:51 +0100

    created gem2 branch

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in EHU-parser_EN_kernel.gemspec
+gemspec
---
+++ lib/EHU-parser_EN_kernel.rb
@@ -0,0 +1,32 @@
+require 'tempfile'
+
+module Opener
+  module Kernel
+    module EHU
+      module Parser	 
+          class EN 
+            VERSION = "0.0.2"
+
+            attr_reader :kernel, :lib
+
+            def initialize
+              core_dir    = File.expand_path("../core", File.dirname(__FILE__))
+
+              @kernel      = core_dir+'/ehu-opennlp-parse-en-1.0.jar'
+            end
+
+            def command(opts={})
+              arguments = opts[:arguments] || []
+              arguments << "-t" if opts[:test]
+
+              "cat #{opts[:input]} | java -jar #{kernel} #{arguments.join(' ')}"
+
+            end
+
+          end
+      end
+    end
+  end
+end
+
+

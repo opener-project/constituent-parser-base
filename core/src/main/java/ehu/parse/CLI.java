5a5139ca4cd45455615c8edd3857178e1650be26
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2014-04-08 14:56:29 +0200

    removing traces

--- core/src/main/java/ehu/parse/CLI.java
+++ core/src/main/java/ehu/parse/CLI.java
@@ -1,19 +1,3 @@
-/*
- *Copyright 2013 Rodrigo Agerri
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-
 package ehu.parse;
 
 
@@ -34,9 +18,8 @@ import ehu.heads.CollinsHeadFinder;
 import ehu.heads.HeadFinder;
 
 /**
- * ehu-parse: Constituent Parsing for 4 languages: English, Spanish, French and Italian
+ * Constituent Parsing for 4 languages: English, Spanish, French and Italian
  * 
- * @author ragerri
  * @version 1.0
  * 
  */
